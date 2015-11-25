79ab87c14c9b4b61d4ef8c2d049e7cdbb19e56e5
NowOrNeverAndroid <noworneverandroid@outlook.es>
2015-11-25 15:21:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/nowor_000/longpresexitwear/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.nowor_000.longpresexitwear;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.DismissOverlayView;
+import android.support.wearable.view.WatchViewStub;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private DismissOverlayView mDismissOverlay;
+    private GestureDetector mDetector;
+
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+
+
+                // Obtain the DismissOverlayView element
+                mDismissOverlay = (DismissOverlayView) stub.findViewById(R.id.dismiss_overlay);
+                mDismissOverlay.setIntroText("long press");
+                mDismissOverlay.showIntroIfNecessary();
+
+            }
+        });
+
+
+
+        // Configure a gesture detector
+        mDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            public void onLongPress(MotionEvent ev) {
+                mDismissOverlay.show();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return mDetector.onTouchEvent(ev) || super.onTouchEvent(ev);
+    }
+
+
+}
+
